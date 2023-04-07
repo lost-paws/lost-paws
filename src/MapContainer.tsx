403d210f6cfa0b61a2acc6cfb8ce9b5ab5bfa2d7
Matt Mattox <bizofmatt@gmail.com>
2023-04-07 09:15:12 -0700

    added styling to container

--- src/MapContainer.tsx
+++ src/MapContainer.tsx
@@ -1,14 +1,15 @@
 import React, { FC, ReactElement } from 'react';
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
+import './MapContainer.css';
+import { borderRadius } from '@mui/system';
 
 
 const MapContainer: FC = () => {
   return (
     <>
-     <Container maxWidth="lg">
-        <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }} />
-        <h1>Hello world</h1>
+     <Container className='container' maxWidth="lg">
+        <Box className='container' sx={{ bgcolor: '#cfe8fc', height: '100vh', borderRadius: '16px'}} />
       </Container>
     </>
   )
