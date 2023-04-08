172c1273bc314cad88ab158011f8bada91b6f042
Nestor Cayanan <nestorcayanan@Nestors-Air.attlocal.net>
2023-04-07 20:41:09 -0500

    login page component finished and hooked up to grab username and password inputs, login button routes to login page

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -6,21 +6,36 @@ import './LoginPage.css'
 
 
 const LoginPage: FC = () => {
+    let username: string;
+    let password: string;
+
+    const handleClick = () => {
+        console.log(username, password)
+    }
+
     return (
         <div className = 'loginform'>
             <Box sx={{
-            width: 250,
-            height: 300,
+            width: 300,
+            height: 350,
             border: '1px solid blue',
             '&:hover': {
-            backgroundColor: 'primary.main',
-            opacity: [0.9, 0.8, 0.7],
+            top: 5,
+            left: 5,
+            boxShadow:'5px 5px'
             }
         }}>
+            <header>
+                <h1>Lost Paws</h1>
+                <h2>Log in to your account</h2>
+            </header>
+            
+
             <div className = 'loginFields'>
-                <TextField label='Username' variant='outlined'></TextField>
-                <TextField label='Password' variant='outlined'></TextField>
-                <Button variant='outlined'>Login</Button>
+                
+                <TextField fullWidth label='Username' variant='outlined' onChange={e => username = e.target.value}></TextField>
+                <TextField fullWidth type='password' label='Password' variant='outlined' onChange={e => password = e.target.value}></TextField>
+                <Button variant='outlined' onClick={() => handleClick()}>Login</Button>
             </div>
                 
             </Box>
